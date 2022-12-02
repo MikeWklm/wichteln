0228f0429dd31135274b62464332b0d6f12d970e
MikeWklm <mw.mikewinkelmann@gmail.com>
2022-12-02 11:12:28 +0100

    do not allow swaps

--- optim_distances.py
+++ optim_distances.py
@@ -1,4 +1,4 @@
-"""Maximization of sum distance between pairs for a fair cultural exchange."""
+"""Maximize sum distance for wichtel pairs without swaps for a fair cultural exchange."""
 import itertools
 from typing import Dict
 
@@ -7,7 +7,7 @@ from geopy.distance import distance
 from geopy.geocoders import Nominatim
 from geopy.location import Location
 from scipy.optimize import Bounds, LinearConstraint, milp
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.preprocessing import OneHotEncoder
 
 # get geo locations of attendees
 geolocator = Nominatim(user_agent='wichteln')
@@ -36,22 +36,36 @@ def get_neg_distance_between_points(loc_a: Location, loc_b: Location) -> float:
     return -distance(lat_lon_a, lat_lon_b).km
 
 # ****** prepare the inputs ******
-# init all possible pairs, with its neg distances (min problem) and one hot encoded attendees
-possible_pairs = list(itertools.combinations(ATTENDEES.keys(), 2))
+# init all possible permutations, with its neg distances (min problem)
+# and one hot encoded givers and getters and unique pairs (a-b and b-a are unique)
+possible_pairs = list(itertools.permutations(ATTENDEES.keys(), 2))
+gives = [giver for giver, _ in possible_pairs]
+gets = [getter for _, getter in possible_pairs]
+unique_pairs = ["".join(sorted(giver + getter)) for giver, getter in possible_pairs]
 pair_distances = np.array([get_neg_distance_between_points(ATTENDEES[a], ATTENDEES[b])
                            for a, b in possible_pairs])
-attendee_matrix = MultiLabelBinarizer().fit_transform(possible_pairs)
+
+ohc_attendees = OneHotEncoder(sparse=False)
+ohc_pairs = OneHotEncoder(sparse=False)
+
+give_matrix = ohc_attendees.fit_transform(np.array(gives)[:, None])
+get_matrix = ohc_attendees.transform(np.array(gets)[:, None])
+unique_pairs = ohc_pairs.fit_transform(np.array(unique_pairs)[:, None])
 
 # ****** formulation of optimization problem using mixed integer linear programming ******
-# minimize the negative pair distances such that every attendee is included once:
+# minimize the negative distances such that every attendee gives and gets a beer
+# and beer swaps are prevented
 
 # our decision variable is binary
 bounds, integrality = Bounds(0,1), 1
-# the transposed row wise sum of attendee matrix is equal to one
-everybody_attends_once = LinearConstraint(attendee_matrix.T, lb=1, ub=1)
+
+everybody_gives_one_beer = LinearConstraint(give_matrix.T, lb=1, ub=1)
+everybody_gets_one_beer = LinearConstraint(get_matrix.T, lb=1, ub=1)
+no_swaps = LinearConstraint(unique_pairs.T, lb=0, ub=1)
+
 # solve
 solution = milp(c=pair_distances, integrality=integrality, bounds=bounds,
-                constraints=[everybody_attends_once])
+                constraints=[everybody_gives_one_beer, everybody_gets_one_beer, no_swaps])
 
 # output result
 idx_pairs = np.argwhere(solution.x == 1).flatten()
